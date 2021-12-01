b946844c38d178516d2bc7329288ca94d0030793
LesterAngulo <lester.licerio@gmail.com>
2021-12-01 11:06:49 -0700

    Se añade componente Card

---
+++ Cinematum-Web/cinematum-app/src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card text-center" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">{{name}}</h5>
+    <p class="card-text">
+          Director: {{ director }}
+          <br />
+          Type: {{ type }}
+          <br />
+          Genrey: {{ genrey }}
+          <br />
+          Year: {{ year }}
+          <br />
+    </p>
+    <button type="button" class="btn btn-outline-primary">Detalle</button>
+    <button type="button" class="btn btn-outline-primary">Editar</button>
+    <button type="button" class="btn btn-outline-primary">Eliminar</button>
+    <button type="button" class="btn btn-outline-primary">Agregar/Quitar favorito</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+   name: {
+      type: String,
+      required: true,
+    },
+    director: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: Number,
+      required: true,
+    },
+    genrey: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Date,
+      required: true,
+    },
+  },
+
+};
+</script>
+
+<style></style>
