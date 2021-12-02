dcf1d7cef73b6013234228064e4a8c20882cbb52
EmanuelNimbuslabs <emanuel@nimbuslabs.mx>
2021-12-01 20:54:26 -0700

    Agregada vista de detalle de film

---
+++ Cinematum-Web/cinematum-app/src/views/Film.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <div class="row">
+        <div class="col">
+            <card class="w-75 mx-auto" :name="movie.Name" :director="movie.Director" :type="movie.type" :genrey="movie.GenreName" :year="movie.year"/>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import card from '../components/Card.vue'
+
+export default {
+    name: "MoviesList",
+    data(){
+        return{
+
+        }
+    },
+    components: {
+        card
+    },
+    methods: {
+        ...mapActions(["getMovieById"]),
+
+    },
+    computed: {
+        ...mapState(["movie"]),
+    },
+    mounted() {
+    },
+    created(){
+        this.getMovieById(this.$route.params.id)
+    }
+ };
+</script>
