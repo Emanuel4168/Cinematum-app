e693906f1cbb846d072560facff0f3ae9d01072e
LesterAngulo <lester.licerio@gmail.com>
2021-12-02 12:23:33 -0700

    Se modifica Store

--- Cinematum-Web/cinematum-app/src/store/index.js
+++ Cinematum-Web/cinematum-app/src/store/index.js
@@ -1,139 +1,141 @@
-import { createStore } from 'vuex'
-import axios from 'axios'
+import axios from 'axios';
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
     movies: [],
     favorites: [],
     genres: [],
-    movie: {}
+    movie: {},
   },
   mutations: {
-    SET_MOVIES(state, movies){
+    SET_MOVIES(state, movies) {
       state.movies = movies;
     },
 
-    SET_FAVORITES(state, favorites){
-      state.movies = favorites;
+    SET_FAVORITES(state, favorites) {
+      state.favorites = favorites;
     },
 
-    SET_GENRES(state, genres){
-      state.movies = genres;
+    SET_GENRES(state, genres) {
+      state.genres = genres;
     },
 
-    SET_MOVIE_Detail(state, movie){
+    SET_MOVIE_Detail(state, movie) {
       state.movie = movie;
-    }
+    },
   },
   actions: {
-    getMovies({commit}){
+    getMovies({ commit }) {
       let baseUrl = 'http://localhost:3000';
       let url = `${baseUrl}/films`;
-      axios.get(url)
-      .then(response => {
-        if(response === undefined){
-          commit('SET_MOVIES', []);
-          return;
-        }
-        commit('SET_MOVIES', response.data);
-      })
-      .catch(error => console.log(error))
+      axios
+        .get(url)
+        .then((response) => {
+          if (response === undefined) {
+            commit('SET_MOVIES', []);
+            return;
+          }
+          commit('SET_MOVIES', response.data);
+        })
+        .catch((error) => console.log(error));
     },
 
-    getFavorites({commit}){
+    getFavorites({ commit }) {
       let baseUrl = 'http://localhost:3000';
       let url = `${baseUrl}/favorites`;
-      axios.get(url)
-      .then(response => {
-        if(response === undefined){
-          commit('SET_FAVORITES', []);
-          return;
-        }
-        commit('SET_FAVORITES', response.data);
-      })
-      .catch(error => console.log(error))
+      axios
+        .get(url)
+        .then((response) => {
+          if (response === undefined) {
+            commit('SET_FAVORITES', []);
+            return;
+          }
+          commit('SET_FAVORITES', response.data);
+        })
+        .catch((error) => console.log(error));
     },
 
-    getGenres({commit}){
+    getGenres({ commit }) {
       let baseUrl = 'http://localhost:3000';
       let url = `${baseUrl}/genres`;
-      axios.get(url)
-      .then(response => {
-        if(response === undefined){
-          commit('SET_GENRES', []);
-          return;
-        }
-        commit('SET_GENRES', response.data);
-      })
-      .catch(error => console.log(error))
+      axios
+        .get(url)
+        .then((response) => {
+          if (response === undefined) {
+            commit('SET_GENRES', []);
+            return;
+          }
+          console.log('campos mamahuevo', response.data);
+          commit('SET_GENRES', response.data);
+        })
+        .catch((error) => console.log(error));
     },
 
-    deleteMovie({commit},id){
-      if(id == ''){
+    deleteMovie({ commit }, id) {
+      if (id == '') {
         return;
       }
       let baseUrl = 'http://localhost:3000';
       let url = `${baseUrl}/films/${id}`;
-      axios.delete(url)
-      .then(response => {
-        
-      })
-      .catch(error => {console.log(error)})
+      axios
+        .delete(url)
+        .then((response) => {})
+        .catch((error) => {
+          console.log(error);
+        });
     },
 
-    postFavorites({commit},favorite){
-      if(favorite == ''){
+    postFavorites({ commit }, favorite) {
+      if (favorite == '') {
         return;
       }
       let baseUrl = 'http://localhost:3000';
       let url = `${baseUrl}/favorites`;
-      axios.post(url,favorite)
-      .then(response => {
-        
-      })
-      .catch(error => console.log(error))
+      axios
+        .post(url, favorite)
+        .then((response) => {})
+        .catch((error) => console.log(error));
     },
 
-    deleteFavorite({commit},id){
-      if(id == ''){
+    deleteFavorite({ commit }, id) {
+      if (id == '') {
         return;
       }
       let baseUrl = 'http://localhost:3000';
       let url = `${baseUrl}/favorites/${id}`;
-      alert(url)
-      axios.delete(url)
-      .then(response => {
-        
-      })
-      .catch(error => console.log(error))
+      alert(url);
+      axios
+        .delete(url)
+        .then((response) => {})
+        .catch((error) => console.log(error));
     },
 
-    postFilm({commit},film){
-      if(film == ''){
+    postFilm({ commit }, film) {
+      if (film == '') {
         return;
       }
       let baseUrl = 'http://localhost:3000';
       let url = `${baseUrl}/films`;
-      axios.post(url,film)
-      .then(response => {
-        
-      })
-      .catch(error => console.log(error))
+      axios
+        .post(url, film)
+        .then((response) => {})
+        .catch((error) => console.log(error));
     },
 
-    getMovieById({commit},id){
-      if(id == ''){
+    getMovieById({ commit }, id) {
+      if (id == '') {
         return;
       }
       let baseUrl = 'http://localhost:3000';
       let url = `${baseUrl}/films/${id}`;
-      axios.get(url)
-      .then(response => {
-        commit('SET_MOVIE_Detail', response.data);
-      })
-      .catch(error => console.log(error))
+      axios
+        .get(url)
+        .then((response) => {
+          commit('SET_MOVIE_Detail', response.data);
+        })
+        .catch((error) => console.log(error));
     },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
