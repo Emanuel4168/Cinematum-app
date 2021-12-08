d8595ab41b0646f0bc337e243dcdc91e1b9cd7de
Daniel Campos <Danielcampossm98@gmail.com>
2021-12-08 13:58:39 -0700

    Nuevas fuentes y estilos

--- Cinematum-Web/cinematum-app/src/App.vue
+++ Cinematum-Web/cinematum-app/src/App.vue
@@ -1,6 +1,12 @@
+
+
 <template>
+
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <div> <h2>&laquo; CINEMATUM &raquo;</h2>
+    <br></div>
+     
+    <router-link to="/">Inicio</router-link> |
     <router-link to="/NewFilm">Agregar Film</router-link> |
     <router-link to="/films">Lista de films</router-link> 
   </div>
@@ -9,24 +15,44 @@
 </template>
 
 <style>
+
+
+@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@700&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+
 }
 
 #nav {
-  padding: 30px;
+  padding: 15px;
+  background: rgb(45, 45, 45);
+  margin-bottom: 10px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: rgb(230, 230, 230);
+ font-family: 'Readex Pro', sans-serif;
+ font-size: 14px;
+
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+#Logo{
+  background: rgb(45, 45, 45);
+}
+
+h2{
+  color: white;
+  font-family: 'Readex Pro', sans-serif;
+  font-size: ;
+}
+
 </style>
